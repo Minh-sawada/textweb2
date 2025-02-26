e4063abe714cb590b937f19392c6bab106084439
PC <PC@DESKTOP-S3UG1DQ>
2025-02-26 17:55:19 +0700

    Add project files.

---
+++ Controllers/trangchuController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace textweb2.Controllers
+{
+    public class trangchuController : Controller
+    {
+        // GET: trangchu
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TheLoai()
+        {
+            return View();
+        }
+        public ActionResult The()
+        {
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="textweb2.MvcApplication" Language="C#" %>
